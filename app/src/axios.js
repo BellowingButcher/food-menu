b8ce71fd6536e487aa06372c86870f10837b15d1
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-10-12 20:33:30 +0000

    Added axios call component
    - tested pulling information
    - tested rendering the information on the page
    - passed both tests

---
+++ app/src/axios.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom/client';
+import axios from 'axios'
+
+function Api () {
+    const baseurl = 'https://astute-baton-362318.ue.r.appspot.com/api/json/';
+    const [data, setData] = useState([]);
+
+    React.useEffect ( () => {
+        axios.get(baseurl).then((response) => {
+            console.log(response);
+            setData(response.data);
+        });
+    }, []);
+
+    if (data.length === 0) return null;
+    // console.log(data[0].title)
+    return (
+        <div>
+            
+            {data.map((item) => {
+                return (<h1>{item.title}</h1>)
+            })}
+        {/* {data[0].title} */}
+        
+        </div>
+    );
+}
+export default Api
+
