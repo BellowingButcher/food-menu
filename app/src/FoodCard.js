cedbe7013c36bbf0aea890a6987935fb35ce3d8c
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-10-13 21:00:54 +0000

    styled food card for card styl

--- app/src/FoodCard.js
+++ app/src/FoodCard.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function FoodCard(props) {
     
     return (
-        
-        <div>
-            <ul>
-                <li>{props.item.title}</li>
-                <li>{props.item.description}</li>
-                <li>{props.item.price}</li>
-            </ul>
+        <div className="container">
+            <div className='row'>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">{props.item.title}</h5>
+                        <p className="card-text">{props.item.description}</p>
+                        <p className="card-text">${props.item.price}</p>
+                    </div>
+                </div>
+            </div>
         </div>
+
     );
 }
 
