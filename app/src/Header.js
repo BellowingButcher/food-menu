977c61f5ab5b2f3f13531795ace5cc047ec8d099
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-10-14 12:49:00 +0000

    Image is displayed and sized correctly

--- app/src/Header.js
+++ app/src/Header.js
@@ -1,4 +1,6 @@
 import React from "react";
+import './index.css'
+
 
 function Header () {
     // let image = require('https://github.com/BellowingButcher/food-menu/blob/mjw/design/app/src/WhaleBacon.jpeg');
@@ -11,7 +13,7 @@ function Header () {
                         <h3>And</h3>
                         <h3>All</h3>
                     </div>
-                    <img className='col' src='' alt="image of a piece of bacon shaped like a whale not found" />
+                    <img className='col' id='bacon' src={require('./pic/WhaleBacon.jpeg')} alt="a piece of bacon shaped like a whale not found" />
                     <div className="col flex-box text-end">
                         <p>Open</p>
                         <p>06:00hrs-11:00hrs</p>
